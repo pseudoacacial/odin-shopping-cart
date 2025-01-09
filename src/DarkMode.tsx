309f7c197ec52d7042c355a512c24f15cc34a3e9
pseudoacacial <aibanzai@protonmail.com>
2025-01-10 00:13:53 +0100

    beautiful tailwind nightmode switcher with changeable w and h :3

--- src/DarkMode.tsx
+++ src/DarkMode.tsx
@@ -26,7 +26,7 @@ export const DarkMode = () => {
 
   return (
     <button onClick={toggleDarkMode} className='cursor-pointer border-slate-800 dark:border-rose-100 border-2 rounded-full w-12 relative'>
-      <div className='rounded-full bg-slate-800 dark:bg-rose-100 size-5 border-slate-80 top-half absolute transition-all duration-500 -translate-x-1/2 -translate-y-1/2 left-1/4 dark:left-3/4'></div>
+      <div className='rounded-full bg-slate-800 dark:bg-rose-100 h-4/5 aspect-square border-slate-80 top-half absolute transition-all duration-500  -translate-y-1/2 left-[2px] dark:left-[calc(100%-2px)] dark:-translate-x-full'></div>
     </button>
   )
 }

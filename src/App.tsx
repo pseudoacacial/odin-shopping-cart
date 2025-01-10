975778d52d2e5cdd68619a0fcfef0927e8b56c9a
pseudoacacial <aibanzai@protonmail.com>
2025-01-10 02:39:30 +0100

    tailwind integrated with vite setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.tailwind.css'
+import './App.css'
 import { Outlet } from 'react-router-dom'
 import { Navbar } from './Navbar'
 

f5e9a57e316795c35eb7400c02bfa8e1ae66f29b
pseudoacacial <aibanzai@protonmail.com>
2025-01-10 15:05:34 +0100

    stale while revalidate!!!

--- src/DarkMode.tsx
+++ src/DarkMode.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { HiOutlineSun, HiOutlineMoon } from "react-icons/hi2";
 
 export const DarkMode = () => {
   // const [browserDarkMode, setBrowserDarkMode] = useState(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
@@ -25,8 +26,12 @@ export const DarkMode = () => {
 
 
   return (
-    <button onClick={toggleDarkMode} className='cursor-pointer border-main border-2 rounded-full w-12 relative'>
-      <div className='rounded-full bg-slate-800 dark:bg-rose-100 h-4/5 aspect-square border-slate-80 top-half absolute transition-all duration-500  -translate-y-1/2 left-[2px] dark:left-[calc(100%-2px)] dark:-translate-x-full'></div>
+    <button onMouseDown={toggleDarkMode} className='cursor-pointer border-main border-2 rounded-full w-12 h-6 relative
+    flex justify-between items-center p-[3px] '>
+      <div className='rounded-full bg-main h-4/5 aspect-square border-slate-80 top-1/2 absolute transition-all duration-500  -translate-y-1/2 left-[2px] dark:left-[calc(100%-2px)] dark:-translate-x-full'></div>
+      <HiOutlineMoon className='transition-all text-rose-100 mt-px'/>
+      <HiOutlineSun className='transition-all text-slate-800'/>
+      
     </button>
   )
 }
--- src/Shop.tsx
+++ src/Shop.tsx
@@ -1,20 +1,24 @@
 import { useState, useEffect } from 'react'
 import { ProductCard, product } from './ProductCard'
+import useSWR , {Fetcher} from 'swr'
 
 export const Shop = () => {
 
   const [products, setProducts] = useState<product[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const fetcher: Fetcher<product[], string> = (url) => fetch(url).then(res=>res.json())
+  
+
+  const {data, error, isLoading } = useSWR('https://fakestoreapi.com/products' ,fetcher)
 
   useEffect(()=>{
-    fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>setProducts(json))
-  },[])
+  },[data])
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
   return (
-    <div>Shop
-        
-        {products.map(x=><ProductCard product={x} />)}
-
+    <div>
+      {data?.map(x=><ProductCard product={x} />)}
     </div>
   )
 }
